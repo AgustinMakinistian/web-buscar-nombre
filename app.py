9bcbbd2ff6a52f3822e71c04c67350454ebe5f01
AgustinMakinistian <165852228+AgustinMakinistian@users.noreply.github.com>
2024-12-08 20:54:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -34,4 +35,6 @@ def index():
     return render_template('index.html', data=[])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Cambiar a un puerto dinámico proporcionado por el entorno de Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
